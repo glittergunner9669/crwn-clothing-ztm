26f3ff19c2f2f671f73ca054aa246f4db627938a
Mabunay <mabunaygo@MNLPREPDSN2WSA.local>
2021-02-15 01:13:30 +0800

    updated our application App component, sign-in component and sign-up component to use hooks

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -21,6 +21,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
       };
     case UserActionTypes.SIGN_IN_FAILURE:
     case UserActionTypes.SIGN_OUT_FAILURE:
+    // eslint-disable-next-line
     case UserActionTypes.SIGN_UP_FAILURE:
       return {
         ...state,
